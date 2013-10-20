72c5f97cb75043d726440dbd7e43b5a1bf029aa1
youngtiger86 <youngtiger86@gmail.com>
2013-10-20 22:42:13 +0800

    update init.sh

--- shell/init.sh
+++ shell/init.sh
@@ -10,6 +10,23 @@ function print_usage()
 	echo "    ./init.sh filename [filename2 ...]"
 }
 
+function create_file()
+{
+	if [ $# -ne 1 ];then
+		echo "Parameter number is wrong."
+		return 1
+	fi
+
+	filename=$1
+	echo "Creating file ${filename}..."
+	echo "#!/bin/bash" > ${filename}
+	echo "# Program:" >> ${filename}
+	echo "#" >> ${filename}
+	echo "# History:" >> ${filename}
+	echo "# `date +%Y/%m/%d`	`whoami`	First release" >> ${filename}
+	echo "File ${filename} created successfully."
+}
+
 if [ $# -lt 1 ];then
 	echo "Name(s) of file should be specified."
 	print_usage
@@ -23,4 +40,19 @@ filenum=$#
 filenames=$@
 echo "filenum: $filenum, file names: $filenames"
 
+for file in $filenames
+do
+	# Check if the file exists already.
+	if [ -f ${file} ];then
+		echo "Warning: file ${file} has already exist. Replacing it?(y/n)"
+		read replace
+		if [ "X$replace" == "Xy" -o "X$replace" == "XY" ];then
+			echo "Files ${file} is replaced."
+		else
+			echo "Info: file ${file} is not created for file with the same name exists."
+			continue
+		fi
+	fi
 
+	create_file ${file}
+done	
